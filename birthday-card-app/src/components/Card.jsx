75a48bd2f4f15ae8bbd9ce0a68fc7f83009369e4
Brittani Lovelace <blovelace84@gmail.com>
2025-09-16 19:48:25 -0400

    Getting started on css styles

--- birthday-card-app/src/components/Card.jsx
+++ birthday-card-app/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Card.css";
 
 function Card() {
   return (
@@ -8,6 +9,7 @@ function Card() {
         Wishing you a day filled with love, laughter, and cake! 💖
       </p>
       <p className="mt-6 text-xl">Love, Your Sister ❤️</p>
+      <p>Brittani</p>
     </div>
   );
 }
